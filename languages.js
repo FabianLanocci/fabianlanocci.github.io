921365881840cde3e5d5be3c72f6af4b73aeed91
Fabian Lanocci <fabianlanocci@gmail.com>
2020-12-22 16:20:31 -0300

    Add icons and implement them in a grid

--- languages.js
+++ languages.js
@@ -5,17 +5,25 @@ var LanguageList = {
   
   //languages Objects
   var WORDS_EN = {
-    "link-home" : "Home",
-    "link-about" : "About me",
-    "link-portfolio" : "Portfolio",
-    "link-contact" : "Contact"
+    "home" : "Home",
+    "about" : "About me",
+    "portfolio" : "Portfolio",
+    "contact" : "Contact",
+    "sub-heading" : "System analyst | Front-end developer",
+    "about-description1" : "Hi! I'm a senior student at Instituto Argentino De La Empresa (IADE) - HILET, currently finishing Superior Technician in Systems (Systems Analyst). ",
+    "about-description2" : "I've been studying HTML, CSS, Javascript and a few other web development concepts/languages by myself as I'm finishing my career and realized I love Front-end developing as much as to code in .NET or Java and now am trying to make the jump from college to the tech industry.",
+    "stack-heading" : "Skills"
   };
   
   var WORDS_ES = {
-    "link-home" : "Inicio",
-    "link-about" : "Sobre mi",
-    "link-portfolio" : "Portafolio",
-    "link-contact" : "Contacto"
+    "home" : "Inicio",
+    "about" : "Sobre mi",
+    "portfolio" : "Portafolio",
+    "contact" : "Contacto",
+    "sub-heading" : "Analista de sistemas | Desarrollador Front-end",
+    "about-description1" : "¡Hola! Soy estudiante de ultimo año en Instituto Argentino De La Empresa (IADE) - HILET, terminando Tecnicatura Superior en Sistemas (Analista de Sistemas).",
+    "about-description2" : "He estado explorando/estudiando HTML, CSS, Javascript y algunos otros conceptos/lenguajes por mi mismo mientras terminaba la carrera y me di cuenta que me encanta el desarrollo en Front-end tanto como codear en .NET o Java asi que ahora estoy aqui para pegar el salto de la universidad a la industria de la tecnología.", 
+    "stack-heading" : "Conocimientos"
   };
   
   
